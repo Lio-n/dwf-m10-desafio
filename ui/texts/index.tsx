86d90d5928ad12d6dcf6a6755bf14db707caa075
Lion <leonardofontan14@gmail.com>
2022-07-19 20:53:36 -0300

    refactor : Change, use 'H1' as base component

--- ui/texts/index.tsx
+++ ui/texts/index.tsx
@@ -1,30 +1,39 @@
 import styled from "styled-components";
 
 type TextProps = {
+  children: string;
   color?: string;
   fontWeight?: "bold" | "regular" | "light" | "thin";
 };
 const weight = { bold: 700, regular: 400, light: 300, thin: 200 };
 
-const styles = `
-  font-weight: ${({ fontWeight = "bold" }) => weight[fontWeight]};
-  color: ${({ color }) => color};
-`;
-
 const H1 = styled.h1<TextProps>`
-  ${styles}
+  font-weight: ${({ fontWeight = "bold" }) => weight[fontWeight]};
+  color: ${({ color }) => color || "#000"};
 `;
 
-const H2 = styled.h2<TextProps>`
-  ${styles}
-`;
+const H2 = ({ children, color, fontWeight }: TextProps) => {
+  return (
+    <H1 as="h2" color={color} fontWeight={fontWeight}>
+      {children}
+    </H1>
+  );
+};
 
-const H3 = styled.h3<TextProps>`
-  ${styles}
-`;
+const H3 = ({ children, color, fontWeight }: TextProps) => {
+  return (
+    <H1 as="h3" color={color} fontWeight={fontWeight}>
+      {children}
+    </H1>
+  );
+};
 
-const Body = styled.p<TextProps>`
-  ${styles}
-`;
+const Body = ({ children, color, fontWeight }: TextProps) => {
+  return (
+    <H1 as="p" color={color} fontWeight={fontWeight}>
+      {children}
+    </H1>
+  );
+};
 
 export { H1, H2, H3, Body };
